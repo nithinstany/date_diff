c0fa1a116cb8f3af141e16b11b5b9318239fa962
isiri <isiri@isiri-System-Product-Name.(none)>
2013-11-26 09:47:28 +0530

    bug fixes

--- lib/date_diff.rb
+++ lib/date_diff.rb
@@ -9,29 +9,29 @@ class Date
     common_year_days_in_month = [ 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
     start_date, end_date = end_date, start_date if start_date > end_date
 
-    start_date_month, start_date_day = start_date.month, start_date.day
-    end_date_month, end_date_day = end_date.month, end_date.day
+    start_date_year, start_date_month, start_date_day = start_date.year, start_date.month, start_date.day
+    end_date_year, end_date_month, end_date_day = end_date.year, end_date.month, end_date.day
 
     day_difference = end_date_day - start_date_day
 
     if (day_difference < 0)
       day_difference = common_year_days_in_month[start_date_month-1] - start_date_day + end_date_day
-      end_date_month = end_date_month -1
+      end_date_month -= 1
     end
 
     if (end_date_month < 0)
-      end_date_month = end_date_month + 12
-      end_date.year = end_date.year -1
+      end_date_month += 12
+      end_date_year -= 1
     end
 
     month_difference = end_date_month - start_date_month
 
     if (month_difference < 0)
-      month_difference = month_difference + 12
-      end_date.year = end_date.year -1
+      month_difference += 12
+      end_date_year -= 1
     end
 
-    year_difference = end_date.year - start_date.year
+    year_difference = end_date_year - start_date_year
 
     difference_hash =  { :year => year_difference, :month => month_difference, :day => day_difference }
 
--- test/test_date_diff.rb
+++ test/test_date_diff.rb
@@ -4,6 +4,7 @@ require 'date'
 class TestDateDiff < MiniTest::Unit::TestCase
 
   def test_date_diff_method
+
     [
       [Date.parse('2013-01-01'), Date.parse('2013-01-01'), {:year => 0, :month => 0, :day => 0, :difference => ""}],
       [Date.parse('2013-01-01'), Date.parse('2013-01-02'), {:year => 0, :month => 0, :day => 1, :difference => "1 day"}],
@@ -15,7 +16,11 @@ class TestDateDiff < MiniTest::Unit::TestCase
       [Date.parse('2000-01-01'), Date.parse('2013-01-01'), {:year => 13, :month => 0, :day => 0, :difference => "13 years"}],
       [Date.parse('2013-08-28'), Date.parse('2014-08-28'), {:year => 1, :month => 0, :day => 0, :difference => "1 year"}],
       
-      [Date.parse('2000-01-01'), Date.parse('2013-06-19'), {:year => 13, :month => 5, :day => 18, :difference => "13 years 5 months 18 days"}]
+      [Date.parse('2000-01-01'), Date.parse('2013-06-19'), {:year => 13, :month => 5, :day => 18, :difference => "13 years 5 months 18 days"}],
+      [Date.parse('2013-11-19'), Date.parse('2013-11-25'), {:year => 0, :month => 0, :day => 6, :difference => "6 days"}],
+      [Date.parse('2012-11-27'), Date.parse('2013-11-25'), {:year => 0, :month => 11, :day => 28, :difference => "11 months 28 days"}]  
+     
+
     ].each do |input|
       assert_equal(Date.diff(input[0], input[1]), input[2])
     end
