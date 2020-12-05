073ee449940f60e3131ab7965780092047b70964
Douglas Pouk <doug@flyonlinetools.com>
2020-12-05 11:54:11 -0600

    add support for dates that cross leap year months, close #2

--- lib/date_diff.rb
+++ lib/date_diff.rb
@@ -1,46 +1,50 @@
-require "date_diff/version"
+require 'date_diff/version'
 require 'rubygems'
 require 'active_support/all'
 require 'i18n'
 
-
 class Date
   def self.diff(start_date, end_date)
-    common_year_days_in_month = [ 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
     start_date, end_date = end_date, start_date if start_date > end_date
 
-    start_date_year, start_date_month, start_date_day = start_date.year, start_date.month, start_date.day
-    end_date_year, end_date_month, end_date_day = end_date.year, end_date.month, end_date.day
+    start_date_year = start_date.year
+    start_date_month = start_date.month
+    start_date_day = start_date.day
+    end_date_year = end_date.year
+    end_date_month = end_date.month
+    end_date_day = end_date.day
 
     day_difference = end_date_day - start_date_day
 
-    if (day_difference < 0)
-      day_difference = common_year_days_in_month[start_date_month-1] - start_date_day + end_date_day
-      end_date_month -= 1
-    end
+    end_date_month -= 1 if day_difference < 0
 
-    if (end_date_month < 0)
+    if end_date_month < 0
       end_date_month += 12
       end_date_year -= 1
     end
 
     month_difference = end_date_month - start_date_month
 
-    if (month_difference < 0)
+    if month_difference < 0
       month_difference += 12
       end_date_year -= 1
     end
 
     year_difference = end_date_year - start_date_year
 
-    difference_hash =  { :year => year_difference, :month => month_difference, :day => day_difference }
-
-  	difference_array = []
-  	["year", "month", "day"].each do |key|
-      value = difference_hash[key.to_sym]
-      difference_array << value.to_s + " " + ((value > 1)? I18n.t(key.to_s.pluralize, :default => key.to_s.pluralize.to_s) : I18n.t(key.to_s, :default =>  key.to_s)).to_s if value > 0
+    if day_difference < 0
+      day_difference = (end_date - (start_date + month_difference.months + year_difference.years)).to_i
+      end_date_month -= 1
     end
-    difference_hash[:difference] = difference_array.join(" ").to_s
+
+    difference_hash = { year: year_difference, month: month_difference, day: day_difference }
+
+    difference_array = []
+    %w[year month day].each do |key|
+      value = difference_hash[key.to_sym]
+      difference_array << value.to_s + ' ' + (value > 1 ? I18n.t(key.to_s.pluralize, default: key.to_s.pluralize.to_s) : I18n.t(key.to_s, default: key.to_s)).to_s if value > 0
+    end
+    difference_hash[:difference] = difference_array.join(' ').to_s
     difference_hash
   end
 end
--- test/test_date_diff.rb
+++ test/test_date_diff.rb
@@ -9,20 +9,24 @@ class TestDateDiff < MiniTest::Unit::TestCase
       [Date.parse('2013-01-01'), Date.parse('2013-01-01'), {:year => 0, :month => 0, :day => 0, :difference => ""}],
       [Date.parse('2013-01-01'), Date.parse('2013-01-02'), {:year => 0, :month => 0, :day => 1, :difference => "1 day"}],
       [Date.parse('2013-01-01'), Date.parse('2013-02-01'), {:year => 0, :month => 1, :day => 0, :difference => "1 month"}],
-      
-      [Date.parse('2013-01-01'), Date.parse('2013-03-03'), {:year => 0, :month => 2, :day => 2, :difference => "2 months 2 days"}],    
+
+      [Date.parse('2013-01-01'), Date.parse('2013-03-03'), {:year => 0, :month => 2, :day => 2, :difference => "2 months 2 days"}],
 
       [Date.parse('2012-01-01'), Date.parse('2013-01-01'), {:year => 1, :month => 0, :day => 0, :difference => "1 year"}],
       [Date.parse('2000-01-01'), Date.parse('2013-01-01'), {:year => 13, :month => 0, :day => 0, :difference => "13 years"}],
       [Date.parse('2013-08-28'), Date.parse('2014-08-28'), {:year => 1, :month => 0, :day => 0, :difference => "1 year"}],
-      
+
       [Date.parse('2000-01-01'), Date.parse('2013-06-19'), {:year => 13, :month => 5, :day => 18, :difference => "13 years 5 months 18 days"}],
       [Date.parse('2013-11-19'), Date.parse('2013-11-25'), {:year => 0, :month => 0, :day => 6, :difference => "6 days"}],
-      [Date.parse('2012-11-27'), Date.parse('2013-11-25'), {:year => 0, :month => 11, :day => 28, :difference => "11 months 28 days"}]  
-     
+      [Date.parse('2012-11-27'), Date.parse('2013-11-25'), {:year => 0, :month => 11, :day => 29, :difference => "11 months 29 days"}],
+      [Date.parse('2020-03-12'), Date.parse('2024-03-01'), {:year => 3, :month => 11, :day => 18, :difference => "3 years 11 months 18 days"}]
 
     ].each do |input|
-      assert_equal(Date.diff(input[0], input[1]), input[2])
+      result = Date.diff(input[0], input[1])
+      # test the hash result
+      assert_equal(result, input[2])
+      # test the values individually
+      assert_equal((input[0] + result[:year].years + result[:month].months + result[:day].days), input[1])
     end
 
   end
